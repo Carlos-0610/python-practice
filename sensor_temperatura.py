cc553e2145f3f807b0f2dc06d919f88a5fcb782d
Carlos Valencia <programmer0610@gmail.com>
2026-03-26 20:37:16 +0000

    Programa interactivo de temperatura

---
+++ sensor_temperatura.py
@@ -0,0 +1,8 @@
+temperatura = input("Ingresa la temperatura: ")
+
+temperatura = int(temperatura)
+
+if temperatura > 30:
+    print("Alerta: temperatura alta")
+else:
+    print("temperatura normal")
